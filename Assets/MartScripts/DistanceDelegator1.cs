831b21ecce6611875603a12b8c6abc24d22a8890
Mart-stack <martolfert@gmail.com>
2025-10-19 18:49:14 +0200

    teleport added

---
+++ Assets/MartScripts/DistanceDelegator1.cs
@@ -0,0 +1,24 @@
+using Oculus.Interaction.Locomotion;
+using UnityEngine;
+
+[RequireComponent(typeof(TeleportInteractor))]
+public class DistanceDelegator : MonoBehaviour
+{
+    private void Awake()
+    {
+        TeleportInteractor interactor = GetComponent<TeleportInteractor>();
+        interactor.AcceptDestination = AcceptDestinationComputer;
+    }
+    bool AcceptDestinationComputer(TeleportInteractable interactable, Pose destination)
+    {
+        return (destination.position - transform.position).magnitude < 4;
+    }
+
+
+
+
+
+}
+
+
+
