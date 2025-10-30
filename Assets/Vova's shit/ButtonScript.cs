e52c115b8c20897e850bfda2f913ce4e01745804
Poturmachina <v.khalkovskyi@gmail.com>
2025-10-30 11:31:12 +0100

    test2

---
+++ Assets/Vova's shit/ButtonScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SimpleReplaceOnClick : MonoBehaviour
+{
+    [SerializeField] private GameObject modelNormal;   // оригинальная модель
+    [SerializeField] private GameObject modelPressed;  // замена
+
+    private bool isPressed = false;
+
+    void Start()
+    {
+        modelNormal.SetActive(true);
+        modelPressed.SetActive(false);
+    }
+
+    // срабатывает, когда на объект кликают (или попадает луч с "Interact" / "Press")
+    private void OnMouseDown()
+    {
+        modelNormal.SetActive(false);
+        modelPressed.SetActive(true);
+        isPressed = true;
+    }
+}
