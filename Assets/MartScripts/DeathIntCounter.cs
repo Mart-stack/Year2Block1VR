22c4afe621e63d91ea1bee485e74387a23356796
Mart-stack <martolfert@gmail.com>
2025-10-30 11:52:26 +0100

    factory added

---
+++ Assets/MartScripts/DeathIntCounter.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DeathAndWinIntCount : MonoBehaviour
+{
+   
+    
+    
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+  
+
+    public static class CountOnDeath
+    {
+        public static int Count = 0;
+    }
+
+    public static class CountOnWin
+    {
+        public static int secondCount = 0;
+    }
+    // Update is called once per frame
+  
+}
